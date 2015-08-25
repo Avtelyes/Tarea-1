f8d6d93c4d17207c1ef43617e720ab6caa3ed9b0
Josue Garcia <avtelyes@gmail.com>
2015-08-25 01:34:29 -0500

    Tarea 1

---
+++ Ejercicio1/Ejercicio1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Ejercicio1
+//
+//  Created by Josué Carlos García Puig on 25/08/15.
+//  Copyright (c) 2015 Avtelyes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
